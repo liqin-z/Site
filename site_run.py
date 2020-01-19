a4d184b75311a0056effbe890c2850672f3039b8
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-01-19 17:18:29 +0800

    Site successfully launched on port 8000

--- site_run.py
+++ site_run.py
@@ -1,20 +1,12 @@
-from app import app
-from configparser import ConfigParser
-import os
-import socket
-import webbrowser
-import time
-import threading
-
-def start_server():
-    print("Server is running at " + time.ctime())
-	app.run(host=config['client']['HOST'], port=config['client']['PORT'], debug=config['client']['debug'], use_reloader=config['client']['use_reloader'])
-
-def start_client():
-    print("Client is running at " + time.ctime())
-	webbrowser.open("http://{0}:{1}".format(config['client']['HOST'],config['client']['PORT']))
-
 if __name__ == "__main__":
+    from app import app
+    from configparser import ConfigParser
+    import os
+    import socket
+    import webbrowser
+    import time
+    import threading
+    
     config = ConfigParser()
     config.read('config.ini')
 
@@ -22,7 +14,12 @@ if __name__ == "__main__":
     # addrs = socket.getaddrinfo(socket.gethostname(), None)
     # Check all users running this site
 
+    def start_server():
+        print("Server is running at " + time.ctime())
+        app.run(host='localhost',port=8000,debug=True,use_reloader=True)
+
+    def start_client():
+        print("Client is running at " + time.ctime())
+        webbrowser.open("http://{0}:{1}".format(config['client']['HOST'],config['client']['PORT']))
+ 
     start_server()
-
-
-    
